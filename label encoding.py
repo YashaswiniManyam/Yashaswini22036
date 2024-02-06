1df1b479e6235439de057740ce6965ca89ec0a8d
YashaswiniManyam <158139605+YashaswiniManyam@users.noreply.github.com>
2024-02-06 13:31:36 +0530

    Add files via upload

---
+++ label encoding.py
@@ -0,0 +1,17 @@
+
+def label_encoding(list):
+    for i in list:
+        list.append()
+        i+=1
+    print(list[i])
+        
+        
+def main():
+    n=int(input("Enter the the number of categories:"))
+    list=[]
+    for i in n:
+        list=input("Enter the values:")
+        list.append()
+    
+    print(list/t/t)
+main() 
