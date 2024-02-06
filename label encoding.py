24152635d857298d4536bfe6a2126cb43e04a02e
YashaswiniManyam <158139605+YashaswiniManyam@users.noreply.github.com>
2024-02-06 23:03:56 +0530

    Add files via upload

--- label encoding.py
+++ label encoding.py
@@ -1,17 +1,20 @@
+def label_encoding():
+    categories=[]
+    encoding_val={}
+    count=0
 
-def label_encoding(list):
-    for i in list:
-        list.append()
-        i+=1
-    print(list[i])
-        
-        
+    num_categories=int(input("Enter the number of categories: "))
+    for _ in range(num_categories):
+        category=input("Enter a category: ")
+        categories.append(category)
+        if category not in encoding_val:
+            encoding_val[category]=count
+            count += 1
+
+    encoded_val=[encoding_val[cat] for cat in categories]
+    return encoded_val
 def main():
-    n=int(input("Enter the the number of categories:"))
-    list=[]
-    for i in n:
-        list=input("Enter the values:")
-        list.append()
-    
-    print(list/t/t)
-main() 
+ encoded_val=label_encoding()
+ print("Encoded values:", encoded_val)
+ 
+main()
